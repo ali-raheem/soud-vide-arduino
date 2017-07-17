95f32876d5bc5f1e2ac58d0932cd982cb131c90d
Ali Raheem <ali.raheem@gmail.com>
2017-07-17 12:35:57 +0100

    Added timer interrupt TPC

--- sous-vide.ino
+++ sous-vide.ino
@@ -19,9 +19,11 @@
 #define BUTTON_UP_PIN   6
 #define BUTTON_DOWN_PIN   7 
 
-
+// A0 is 14.
 #define TEMP_PIN  A0
-#define PID_PIN   A1
+#define PID_PIN   3
+
+#define TPC_WINDOW 5000
 
 struct settings_t {
   double Kp, Ki, Kd;
@@ -35,50 +37,82 @@ double output = 0;
 unsigned int buttons = 0;
 
 PID myPID(&temp, &output, &target, 0, 0, 0, P_ON_M, DIRECT);
+ISR(TIMER2_OVF_vect) {
+  // Will have to be a TPC https://playground.arduino.cc/Code/PIDLibraryRelayOutputExample
+  // To enable slow relay or SSR (zero-crossings) to function.
+  static unsigned long startTime;
+  unsigned long now = millis();
+  if (TPC_WINDOW < now - startTime) {
+    startTime = now;
+  }
+  digitalWrite(PID_PIN, (now - startTime < output));
+  TCNT2 = 0;
+  TIFR2 = 0;
+}
 
 void setup() {
-// Serial.
-// setup
+//  noInterrupts();
+  TIMSK2 = 0;
+  TCCR2B = 0;
+  TCNT2 = 0;
+  TIFR2 = 0;
+  TCCR2A = 0; // COUNTER "NORMAL" mode.
+  TCCR2B = (1 << CS22) + (1 << CS21) + (1 << CS20); // 255 * 1024/16MHz = 16ms
+  TIMSK2 = (1 << TOIE2);
+//  interrupts();
+  
+  eeprom_read_block((void *) &settings, (void *) 0, sizeof(settings));
 
-  eeprom_read_block((void *)&settings, (void *) 0, sizeof(settings));
-
-  pinMode(BUTTON_UP_PIN, INPUT); //etc...
-  digitalWrite(BUTTON_UP_PIN, HIGH); // pull up
-  pinMode(TEMP_PIN, INPUT);
-
+  setupPins();
+    
   myPID.SetTunings(settings.Kp, settings.Ki, settings.Kd);
+  myPID.SetOutputLimits(0, TPC_WINDOW);
   disablePID();
   mode = STANDBY;
+}
+
+inline void setupPins() {
+  pinMode(BUTTON_UP_PIN, INPUT);
+  digitalWrite(BUTTON_DOWN_PIN, HIGH);
+  pinMode(BUTTON_DOWN_PIN, INPUT);
+  digitalWrite(BUTTON_RIGHT_PIN, HIGH);
+  pinMode(BUTTON_RIGHT_PIN, INPUT);
+  digitalWrite(BUTTON_RIGHT_PIN, HIGH);
+  pinMode(BUTTON_LEFT_PIN, INPUT);
+  digitalWrite(BUTTON_LEFT_PIN, HIGH);
+  pinMode(TEMP_PIN, INPUT);
+  pinMode(PID_PIN, OUTPUT);
 }
 
 void disablePID() {
   myPID.SetMode(MANUAL);
   output = 0;
+  TIMSK2 = 0;
+}
+
+void enablePID() {
+  myPID.SetMode(AUTOMATIC);
+  TIMSK2 = (1 << TOIE2);
 }
 
 void getTemp() {
+  // Prolly will end up with a onewire temp probe, will be asynchronous readings.
+  // if temp ready read and start a new conversion,
+  // otherwise return.
   int i = analogRead(TEMP_PIN);
   temp = i/60 *32; // or some shit.
 }
-void doOutput() {
-  // This will likely become a Timer ISR using 
-  analogWrite(PID_PIN, output);
-}
 
-unsigned int getButtons() {
-  int buttons = 0;
+
+void getButtons() {
+  buttons = 0;
   buttons &= digitalRead(BUTTON_LEFT_PIN);
   buttons &= digitalRead(BUTTON_RIGHT_PIN);
   buttons &= digitalRead(BUTTON_UP_PIN);
   buttons &= digitalRead(BUTTON_DOWN_PIN);
-  return buttons;
 }
 
 void err() {
-// disable output
-// log time **
-// await reset
-// disable interupts
   disablePID();
 }
 
@@ -123,14 +157,13 @@ void autoTune() {
   autoTuner.SetControlType(1); // PID
   do {
     getTemp();
-    doOutput();
   } while(0 == autoTuner.Runtime());
   myPID.SetTunings(autoTuner.GetKp(), autoTuner.GetKp(), autoTuner.GetKd());
   mode = STANDBY;
 }
 
 void cook() {
-  myPID.SetMode(AUTOMATIC);
+  enablePID();
   // print display
   if(buttons & BUTTON_LEFT)
     mode = CONFIG;
@@ -140,7 +173,6 @@ void loop() {
   getTemp();
   if (AUTOMATIC == myPID.GetMode()) {
     myPID.Compute();
-    doOutput();
   }
   getButtons();
   switch (mode) {
